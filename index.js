cc04d99127f67971da998ca40c92857544bfc8b9
mvdgragt <michiel@vandergragt.eu>
2023-04-04 17:53:27 +0200

    allow cors

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
+let allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', "*");
+    next();
+  }
+  app.use(allowCrossDomain);
+
 // Set headers for preflight request
 // app.options('/onloan', {mode:'cors'});
 
