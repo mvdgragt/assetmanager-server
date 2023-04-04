6e48632011356209247798e4fe1c9e12cb66187e
mvdgragt <michiel@vandergragt.eu>
2023-04-04 17:46:38 +0200

    allow cors

--- index.js
+++ index.js
@@ -6,13 +6,7 @@ const middleware = require('./src/middleware/index');
 const port = process.env.PORT || 5000;
 // const bodyParser = require('body-parser')
 
-app.use cors(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
+app.use(require('cors')())
 
 // Set headers for preflight request
 // app.options('/onloan', {mode:'cors'});
