142e55e14d04c508c8058e2705202c01591d2d25
mvdgragt <michiel@vandergragt.eu>
2023-04-04 14:12:45 +0200

    copied from previous file

--- db.js
+++ db.js
@@ -7,6 +7,10 @@ const mysql = require("mysql2")
 //     database: "assetmanager"
 // });
 
+const urlDB = `mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway`
+
+const pool = mysql.createPool(urlDB);
+
 // const pool = mysql.createPool({
 //     host: `${process.env.DB_HOST}`,
 //     user: `${process.env.DB_USER}`,
@@ -14,7 +18,6 @@ const mysql = require("mysql2")
 //     database: `${process.env.DB_DATABASE}`
 // });
 
-const pool = mysql.createPool(`${process.env.MySQL.MYSQL_URL}`);
 
 let sql2 = "SELECT SerialNumber FROM movements INNER JOIN assets ON movements.AssetID = assets.id INNER JOIN persons ON movements.PersonID = persons.id WHERE persons.FirstName = 'PYP' AND persons.LastName = '6' AND movements.BookInDate IS NULL AND assets.AssetTypeID=5"
 
