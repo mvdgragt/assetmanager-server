504ca52323cb5f10663d485d5a2e3adb40279be9
mvdgragt <michiel@vandergragt.eu>
2023-04-04 17:45:03 +0200

    allow cors

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const middleware = require('./src/middleware/index');
 const port = process.env.PORT || 5000;
 // const bodyParser = require('body-parser')
 
-app.use(function(req, res, next) {
+app.use cors(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
