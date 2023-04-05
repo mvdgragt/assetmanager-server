e7ac332e9a28135d56828217aac86c5354805263
mvdgragt <michiel@vandergragt.eu>
2023-04-05 21:04:43 +0200

    adding headers to fetch

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-
-const mysql = require("mysql2")
+require('dotenv').config();
+const mysql = require('mysql2');
 
 
 
@@ -18,16 +18,20 @@ const mysql = require("mysql2")
 //     database: "railway"
 // });
 
- const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
-
-const pool = mysql.createPool(urlDB);
-
 // const pool = mysql.createPool({
-//     host: `${process.env.DB_HOST}`,
-//     user: `${process.env.DB_USER}`,
-//     password: `${process.env.DB_PASSSWORD}`,
-//     database: `${process.env.DB_DATABASE}`
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSSWORD,
+//     database: process.env.DB_DATABASE,
 // });
+
+// const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
+
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+
+ const pool = mysql.createPool(urlDB);
+
+
 
 
 
