c288143d2bfdd9e6efabf3e384c6eacc80cb4ec5
mvdgragt <michiel@vandergragt.eu>
2023-04-05 17:33:42 +0200

    adding headers to fetch

--- db.js
+++ db.js
@@ -1,10 +1,34 @@
 
 const mysql = require("mysql2")
 
- const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
-
-const pool = mysql.createPool('mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway');
 
 
+const pool = mysql.createPool({
+    host: 'containers-us-west-55.railway.app',
+    user: 'root',
+    password: '5NOSSCDlL9LV2b1svlOH',
+    database: 'railway',
+    port: 6016
+});
 
-module.exports = pool.promise();
+// const pool = mysql.createPool({
+//     host: "containers-us-west-55.railway.app",
+//     user: "root",
+//     password: "5NOSSCDlL9LV2b1svlOH",
+//     database: "railway"
+// });
+
+//  const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
+
+// const pool = mysql.createPool(urlDB);
+
+// const pool = mysql.createPool({
+//     host: `${process.env.DB_HOST}`,
+//     user: `${process.env.DB_USER}`,
+//     password: `${process.env.DB_PASSSWORD}`,
+//     database: `${process.env.DB_DATABASE}`
+// });
+
+
+
+module.exports = pool;
