aafedf09682c81522a6ef3f098e9ad926b86782d
mvdgragt <michiel@vandergragt.eu>
2023-04-05 12:16:38 +0200

    dotenv

--- db.js
+++ db.js
@@ -1,5 +1,6 @@
-import * as dotenv from 'dotenv' 
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
 dotenv.config()
+require('dotenv').config()
 
 const mysql = require("mysql2")
 
