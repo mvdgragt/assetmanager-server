da374b5fedfd2cee6921baa53a48762624e1b4f1
mvdgragt <michiel@vandergragt.eu>
2023-04-05 13:35:06 +0200

    trying pool

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
-//require("dotenv").config()
+// require("dotenv").config()
 
 const mysql = require("mysql2")
 
-// const pool = mysql.createPool({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "assetmanager"
-// });
+const pool = mysql.createPool({
+    host: "containers-us-west-55.railway.app",
+    user: "root",
+    password: "5NOSSCDlL9LV2b1svlOH",
+    database: "railway"
+});
 
 // const pool = mysql.createPool({
 //     host: "containers-us-west-55.railway.app",
@@ -16,9 +16,9 @@ const mysql = require("mysql2")
 //     database: "railway"
 // });
 
- const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
+ //const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
 
-const pool = mysql.createPool(urlDB);
+// const pool = mysql.createPool(urlDB);
 
 // const pool = mysql.createPool({
 //     host: `${process.env.DB_HOST}`,
