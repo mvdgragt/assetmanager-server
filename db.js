a40707ede610f85531cc3ece14ebef2db55960e9
mvdgragt <michiel@vandergragt.eu>
2023-04-05 21:23:35 +0200

    adding headers to fetch

--- db.js
+++ db.js
@@ -27,7 +27,7 @@ const mysql = require('mysql2');
 
 // const urlDB = "mysql://root:5NOSSCDlL9LV2b1svlOH@containers-us-west-55.railway.app:6016/railway"
 
-const urlDB = `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${process.env.MYSQL_HOST}:${process.env.MYSQL_PORT}/${process.env.MYSQL_DATABASE}`
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
 
  const pool = mysql.createPool(urlDB);
 
