7efb9454cc9edcc263a9c07aa11c6279174f87f7
mvdgragt <michiel@vandergragt.eu>
2023-04-06 09:59:15 +0200

    added files

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ const middleware = require('./src/middleware/index');
 // const bodyParser = require('body-parser')
 
 //middleware
-// 
+app.use(cors());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
-
-  
 app.use(express.json()); //req.body
 app.use(middleware.decodeToken);
 
