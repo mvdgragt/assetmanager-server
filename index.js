cfb2d4d90aff6310eaceb7ccd0c026bafa7a2904
mvdgragt <michiel@vandergragt.eu>
2023-04-10 15:07:38 +0200

    problems with req.body

--- index.js
+++ index.js
@@ -224,10 +224,10 @@ app.delete("/deleteasset/:SerialNumber", async (req,res) => {
 // })
 
 app.post("/newMovement/", async (req,res) => {
-    const {data} = req.body;
-    console.log(data)
+    const {chosenPersonID, chosenDeviceID} = req.body;
         try {
-    
+            console.log("data :", chosenPersonID)
+
             const updateMovements = await pool.query("INSERT INTO movements (BookOutDate, BookInDate, PersonID, AssetID, MovementDescription) VALUES (CURRENT_TIMESTAMP(), NULL, ?, ?, ?)", [chosenPersonID, chosenDeviceID])
             res.json(updateMovements)
         } catch (err) {
