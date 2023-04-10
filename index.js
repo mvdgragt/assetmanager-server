aee597d0346cf705642cf7f39a35ae0d59355b43
mvdgragt <michiel@vandergragt.eu>
2023-04-10 22:28:33 +0200

    trying to fix NaN

--- index.js
+++ index.js
@@ -226,12 +226,13 @@ app.delete("/deleteasset/:SerialNumber", async (req,res) => {
 app.post("/newMovement", async (req,res) => {
         try {
             const {chosenPersonID, chosenDeviceID} = req.body;
-            const chosenPersonIDNum = Number(chosenPersonID);
-            const chosenDeviceIDNum = Number(chosenDeviceID);
+   //         const chosenPersonIDNum = Number(chosenPersonID);
+   //         const chosenDeviceIDNum = Number(chosenDeviceID);
 
             console.log("chosenPersonID:", chosenPersonID);
             console.log("chosenDeviceID:", chosenDeviceID);
-            const updateMovements = await pool.query("INSERT INTO movements (BookOutDate, BookInDate, PersonID, AssetID, MovementDescription) VALUES (NOW(), NULL, ?, ?, "-")", [chosenPersonIDNum, chosenDeviceIDNum])
+//            const updateMovements = await pool.query("INSERT INTO movements (BookOutDate, BookInDate, PersonID, AssetID, MovementDescription) VALUES (NOW(), NULL, ?, ?, "-")", [chosenPersonIDNum, chosenDeviceIDNum])
+            const updateMovements = await pool.query("INSERT INTO movements (BookOutDate, BookInDate, PersonID, AssetID, MovementDescription) VALUES (NOW(), NULL, ?, ?, "-")", [chosenPersonID, chosenDeviceID])
             res.json(updateMovements)
           //  console.log(req.body)
         } catch (err) {
